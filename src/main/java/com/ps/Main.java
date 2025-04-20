5886692f8956e4d34a191cfe1a3d7d66c6a2fe81
selina <ss@ss.com>
2025-04-20 11:11:08 -0400

    dislayed home screen

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,16 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void displayHomeScreen() {
+        System.out.println("Home Screen");
+        System.out.println("Select one of the options");
+        System.out.println("1: Show Available Books");
+        System.out.println("2: Show Checked Out Books");
+        System.out.println("0: Exit");
     }
 }
