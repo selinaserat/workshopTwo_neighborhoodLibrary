0ab6325faf2d07818b58bb99634768960ee0b17a
selina <ss@ss.com>
2025-04-20 11:34:05 -0400

    displayed home screen updated  and asked for  user input

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -8,9 +8,16 @@ public class Main {
 
     public static void displayHomeScreen() {
         System.out.println("Home Screen");
-        System.out.println("Select one of the options");
         System.out.println("1: Show Available Books");
         System.out.println("2: Show Checked Out Books");
         System.out.println("0: Exit");
+        System.out.print("Select one of the options: ");
+    }
+
+    public static void main(String[] args) {
+
+        displayHomeScreen();
+        int givenCommand = scanner.nextInt();
+
     }
 }
